3e6a62cf39ed51daff95c37c161ce10513301a79
saloni-gupta27 <guptasaloni.2016@gmail.com>
2025-11-04 15:45:24 +0530

    commenting additional js file

--- src/components/r.js
+++ src/components/r.js
@@ -1,14 +1,14 @@
-var a = 5;
-function first() {
-a = 6;
-console.log(a);
+// var a = 5;
+// function first() {
+// a = 6;
+// console.log(a);
 
-}
+// }
 
-console.log("from outside", a);
+// console.log("from outside", a);
 
-function second() {
-console.log(a);
+// function second() {
+// console.log(a);
 
-}
-second();
+// }
+// second();
